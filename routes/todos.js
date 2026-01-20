f7930e0506b662bc4ce969ca01b4b2afec21b532
Codefitness21 <kelly.m.hill2115@gmail.com>
2026-01-20 13:49:01 -0600

    updated css

--- routes/todos.js
+++ routes/todos.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const todosController = require('../controllers/todos')
 
+//organizes requests coming in and want to be able to pass those off to individual controllers.
 router.get('/', todosController.getTodos)
 
 router.post('/createTodo', todosController.createTodo)
