3af9224f30b7293188e491c9f08a63f6204dfb0b
Codefitness21 <kelly.m.hill2115@gmail.com>
2026-01-22 20:56:35 -0600

    added open and closed nav for recipe search overlay

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,9 @@ const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('span.not')
 const todoComplete = document.querySelectorAll('span.completed')
 
+document.querySelector('.add-recipe-btn').addEventListener('click', openNewRecipeBook)
+document.querySelector('.close-btn').addEventListener('click', closeNewRecipeBook)
+
 Array.from(deleteBtn).forEach((el)=>{
     el.addEventListener('click', deleteTodo)
 })
@@ -13,6 +16,14 @@ Array.from(todoItem).forEach((el)=>{
 Array.from(todoComplete).forEach((el)=>{
     el.addEventListener('click', markIncomplete)
 })
+
+function openNewRecipeBook(){
+    document.getElementById('search-overlay').style.height = "40%"
+}
+function closeNewRecipeBook(){
+    document.getElementById('search-overlay').style.height = "0%"
+}
+
 
 async function deleteTodo(){
     const todoId = this.parentNode.dataset.id
